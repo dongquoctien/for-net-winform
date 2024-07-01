0b16fc31d43fdb8fd4a371f6b919816968702b6f
Đồng Quốc Tiến <itdongquoctien@gmail.com>
2024-07-01 19:06:36 +0700

    update code

---
+++ DownloadGGDriverPDF/WinFormsApp1/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WinFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            txtURL = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            btnDownload = new System.Windows.Forms.Button();
+            btnExportPDF = new System.Windows.Forms.Button();
+            progressBar1 = new System.Windows.Forms.ProgressBar();
+            txtName = new System.Windows.Forms.TextBox();
+            label2 = new System.Windows.Forms.Label();
+            lblLoading = new System.Windows.Forms.Label();
+            txtPage = new System.Windows.Forms.TextBox();
+            label3 = new System.Windows.Forms.Label();
+            backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            flowLayoutPanel1.AutoScroll = true;
+            flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            flowLayoutPanel1.Location = new System.Drawing.Point(0, 334);
+            flowLayoutPanel1.Name = "flowLayoutPanel1";
+            flowLayoutPanel1.Size = new System.Drawing.Size(800, 156);
+            flowLayoutPanel1.TabIndex = 0;
+            // 
+            // txtURL
+            // 
+            txtURL.Location = new System.Drawing.Point(73, 12);
+            txtURL.Name = "txtURL";
+            txtURL.Size = new System.Drawing.Size(511, 23);
+            txtURL.TabIndex = 0;
+            txtURL.Text = "https://drive.google.com/drive/folders/15TDlbPq5XJhjVt3VYmAQuOZHTb5pCLcM";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(14, 15);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(53, 15);
+            label1.TabIndex = 2;
+            label1.Text = "Link PDF";
+            // 
+            // btnDownload
+            // 
+            btnDownload.Location = new System.Drawing.Point(590, 12);
+            btnDownload.Name = "btnDownload";
+            btnDownload.Size = new System.Drawing.Size(95, 55);
+            btnDownload.TabIndex = 2;
+            btnDownload.Text = "Download";
+            btnDownload.UseVisualStyleBackColor = true;
+            btnDownload.Click += btnDownload_ClickAsync;
+            // 
+            // btnExportPDF
+            // 
+            btnExportPDF.Enabled = false;
+            btnExportPDF.Location = new System.Drawing.Point(693, 11);
+            btnExportPDF.Name = "btnExportPDF";
+            btnExportPDF.Size = new System.Drawing.Size(95, 56);
+            btnExportPDF.TabIndex = 3;
+            btnExportPDF.Text = "Export PDF";
+            btnExportPDF.UseVisualStyleBackColor = true;
+            btnExportPDF.Click += btnExportPDF_ClickAsync;
+            // 
+            // progressBar1
+            // 
+            progressBar1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            progressBar1.Location = new System.Drawing.Point(0, 491);
+            progressBar1.Name = "progressBar1";
+            progressBar1.Size = new System.Drawing.Size(800, 10);
+            progressBar1.TabIndex = 4;
+            // 
+            // txtName
+            // 
+            txtName.Location = new System.Drawing.Point(217, 44);
+            txtName.Name = "txtName";
+            txtName.Size = new System.Drawing.Size(367, 23);
+            txtName.TabIndex = 1;
+            txtName.Text = "giáo trình hán ngữ quyển 1 tiếng việt.pdf";
+            txtName.KeyPress += txtPage_KeyPress;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(148, 47);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(63, 15);
+            label2.TabIndex = 2;
+            label2.Text = "Name PDF";
+            // 
+            // lblLoading
+            // 
+            lblLoading.AutoSize = true;
+            lblLoading.Location = new System.Drawing.Point(12, 72);
+            lblLoading.Name = "lblLoading";
+            lblLoading.Size = new System.Drawing.Size(0, 15);
+            lblLoading.TabIndex = 5;
+            // 
+            // txtPage
+            // 
+            txtPage.Location = new System.Drawing.Point(73, 44);
+            txtPage.Name = "txtPage";
+            txtPage.Size = new System.Drawing.Size(57, 23);
+            txtPage.TabIndex = 1;
+            txtPage.Text = "175";
+            txtPage.KeyPress += txtPage_KeyPress;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new System.Drawing.Point(14, 47);
+            label3.Name = "label3";
+            label3.Size = new System.Drawing.Size(38, 15);
+            label3.TabIndex = 2;
+            label3.Text = "Pages";
+            // 
+            // backgroundWorker1
+            // 
+            backgroundWorker1.DoWork += backgroundWorker1_DoWork;
+            backgroundWorker1.ProgressChanged += backgroundWorker1_ProgressChanged;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new System.Drawing.Point(0, 90);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new System.Drawing.Size(800, 238);
+            dataGridView1.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(800, 501);
+            Controls.Add(dataGridView1);
+            Controls.Add(lblLoading);
+            Controls.Add(progressBar1);
+            Controls.Add(btnExportPDF);
+            Controls.Add(btnDownload);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(txtPage);
+            Controls.Add(txtName);
+            Controls.Add(txtURL);
+            Controls.Add(flowLayoutPanel1);
+            Name = "Form1";
+            Text = "Download GG";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDownload;
+        private System.Windows.Forms.Button btnExportPDF;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblLoading;
+        private System.Windows.Forms.TextBox txtPage;
+        private System.Windows.Forms.Label label3;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ DownloadGGDriverPDF/WinFormsApp1/Form1.cs
@@ -0,0 +1,288 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools;
+using OpenQA.Selenium.Interactions;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Web;
+using System.Windows.Forms;
+using static System.Windows.Forms.Design.AxImporter;
+
+namespace WinFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void txtPage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Check if the pressed key is not a digit and is not a control key (like backspace)
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                // If it's not, suppress the key press event
+                e.Handled = true;
+            }
+        }
+
+        private async void btnDownload_ClickAsync(object sender, EventArgs e)
+        {
+
+
+            progressBar1.Value = 0;
+            progressBar1.Maximum = 100;
+            backgroundWorker1.RunWorkerAsync();
+        }
+
+        private async void StartDownloadImg(string url, int pageNumber)
+        {
+
+            List<string> imageUrls = new List<string>();
+
+            // Replace with your image URLs
+
+            // string url = "https://drive.google.com/viewer2/prod-01/img?ck=drive&ds=APznzaYZP6erlpM7wCFIYiFJATvEiN560edVHU0l2B5ce2v7-_Dr9X9e7PBH8yLG1OL1DLrIBf1yRMka7U3aGyQqQ2Mof_Z_9kfrhKBBv1c-p-E30joRXNWb2_5VWbx1H-7QLoR3fX-gGr8kgF92MOB7xawrThgyl3WOJgbR6gLz5UWnMcsbRAsiJK45VXJ-Pi44whlYGLFUf-9F4FrNCU1GHydU6ch8yVwW4zdfdjX3uIbsjWa-3Z1cBX2Z7XjqwnOWYStAlZIUpJFk12zW9S_5nKHFGOpRsSILQB6WJuk9p1M_YkaNLHubE4PDH-XjErniLaJNVxGKlJZ2q_uNHt8iSWQnPBG0ETBB6hzEkyQWklymOSwYelCmrsq-EY_zbbIlHFbJTD_fVt0AwV_svcXhyWLWufoFCYbBcureR-gsUiUjZezXdI0%3D&authuser=0&page=0&skiphighlight=true&w=1600&webp=true";
+
+            // Create a DataTable
+            DataTable dataTable = new DataTable();
+            dataTable.Columns.Add("String Data", typeof(string)); // Assuming you want a single column named "String Data"
+
+
+            // Parse the URL
+            UriBuilder uriBuilder = new UriBuilder(url);
+            NameValueCollection queryParameters = HttpUtility.ParseQueryString(uriBuilder.Query);
+
+            for (int i = 0; i < pageNumber; i++)
+            {
+                // Modify the page parameter
+                queryParameters["page"] = i.ToString(); // Change to whatever page number you need
+                // Update the query string in the UriBuilder
+                uriBuilder.Query = queryParameters.ToString();
+
+
+                imageUrls.Add(uriBuilder.ToString());
+
+                dataTable.Rows.Add(uriBuilder.ToString());
+            }
+
+
+            // Bind the DataTable to the DataGridView
+            dataGridView1.DataSource = dataTable;
+
+        }
+
+        private bool IsValidUrl(string url)
+        {
+            string pattern = @"^(http|https|ftp)://[^\s/$.?#].[^\s]*$";
+            Regex regex = new Regex(pattern, RegexOptions.Compiled | RegexOptions.IgnoreCase);
+            return regex.IsMatch(url);
+        }
+
+        private bool IsValidPageNumber(string pageNumber)
+        {
+            return int.TryParse(pageNumber, out _);
+        }
+        private async Task<System.Drawing.Image> DownloadImageAsync(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode();
+
+                    using (Stream stream = await response.Content.ReadAsStreamAsync())
+                    {
+                        return System.Drawing.Image.FromStream(stream);
+                    }
+                }
+                catch (HttpRequestException e)
+                {
+                    MessageBox.Show($"Request error: {e.Message}");
+                    return null;
+                }
+                catch (IOException e)
+                {
+                    MessageBox.Show($"IO error: {e.Message}");
+                    return null;
+                }
+            }
+        }
+
+        private void RunSelenium(string url, int pageNumber, string name, System.ComponentModel.DoWorkEventArgs e)
+        {
+
+            // var isFindViewer = false;
+            var options = new ChromeOptions();
+            //   options.AddArgument("--headless");
+            options.AddArgument("--disable-gpu");
+            //   options.AddArgument("start-maximized"); // Maximize browser window on startup
+            //   options.AddArgument("disable-infobars"); // Disable info bars that cover the browser window
+            options.AddExcludedArgument("disable-popup-blocking");
+
+            //  progressBar1.Maximum = pageNumber;
+
+            using (var driver = new ChromeDriver(options))
+            {
+
+                // _ =   GetReponseFromByNetworkAsync(driver);
+                GetReponseFromByNetworkAsync(driver, pageNumber, e);
+
+
+                driver.Navigate().GoToUrl(url);
+
+
+                // Find the div element by its text content using XPath
+
+                // Execute JavaScript using IJavaScriptExecutor
+                IJavaScriptExecutor js = (IJavaScriptExecutor)driver;
+
+                // Your JavaScript code
+                string script = $@"
+                    var aTags = document.getElementsByTagName('div');
+                    var searchText = '{name}';
+                    var found;
+
+                    for (var i = 0; i < aTags.length; i++) {{
+                        if (aTags[i].textContent.trim() === searchText) {{
+                            found = aTags[i];
+                            found.click();
+                            break;
+                        }}
+                    }}
+            ";
+                // Execute the script
+                js.ExecuteScript(script);
+
+
+                System.Threading.Thread.Sleep(15000); // Adjust as needed
+
+                //   driver.Quit();
+            }
+
+
+        }
+
+        private async void GetReponseFromByNetworkAsync(ChromeDriver driver, int pageNumber, System.ComponentModel.DoWorkEventArgs e)
+        {
+            var isFindViewer = false;
+            var session = ((IDevTools)driver).GetDevToolsSession();
+            var domains = session.GetVersionSpecificDomains<OpenQA.Selenium.DevTools.V126.DevToolsSessionDomains>();
+
+
+            var getHotelRoomListRequestId = "";
+            var url = "";
+            // Request ID get Data
+            domains.Network.ResponseReceived += (_, response) =>
+            {
+                e.Result = response.Response.Url;
+                //  Console.WriteLine($"ResponseReceived {response.RequestId} => {response.Response.Url} ");
+                if (response.Response.Url.Contains("https://drive.google.com/viewer2/prod-01/img?ck=drive"))
+                {
+                    if (!isFindViewer)
+                    {
+                        getHotelRoomListRequestId = response.RequestId;
+                        isFindViewer = true;
+
+                        StartDownloadImg(response.Response.Url, pageNumber);
+                    }
+
+                }
+            };
+
+            domains.Network.LoadingFinished += async (_, response) =>
+            {
+                var requestID = response.RequestId;
+                e.Result = $"LoadingFinished => {requestID} ";
+                if (getHotelRoomListRequestId == requestID)
+                {
+                    try
+                    {
+                        var responseBody = await domains.Network.GetResponseBody(new OpenQA.Selenium.DevTools.V126.Network.GetResponseBodyCommandSettings
+                        {
+                            RequestId = requestID
+                        });
+                        e.Result = $"Loading Finished Body => {requestID} ";
+                        //   e.Result  = $"Loading Finished Body => {responseBody.Body} ";
+                    }
+                    catch (Exception ex)
+                    {
+                        e.Result = $"Loading Fail: {requestID}";
+                        //   Console.WriteLine($"Loading Finished({requestID}) fail: {ex.Message}");
+                    }
+                }
+
+            };
+            _ = await domains.Network.Enable(new OpenQA.Selenium.DevTools.V126.Network.EnableCommandSettings() { });
+
+        }
+
+        private void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            string url = txtURL.Text;
+            string pageNumber = txtPage.Text;
+            string name = txtName.Text;
+            btnExportPDF.Enabled = false;
+
+            if (IsValidUrl(url) && IsValidPageNumber(pageNumber) && !string.IsNullOrEmpty(name))
+            {
+                RunSelenium(url, int.Parse(pageNumber), name, e);
+
+            }
+            else
+            {
+                MessageBox.Show("Vui lòng nhập Link và Page, Name đúng, không được để trống.");
+            }
+
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, System.ComponentModel.ProgressChangedEventArgs e)
+        {
+            // Update UI with the result from background operation
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private async void btnExportPDF_ClickAsync(object sender, EventArgs e)
+        {
+            //flowLayoutPanel1.Controls.Clear();
+
+
+            //foreach (string imgUrl in imageUrls)
+            //{
+            //    // lblLoading.Text = $"Show: " + imgUrl;
+
+            //    PictureBox pb = new PictureBox();
+            //    pb.SizeMode = PictureBoxSizeMode.StretchImage;
+            //    pb.Width = 160;
+            //    pb.Height = 227;
+
+            //    Image image = await DownloadImageAsync(imgUrl);
+            //    if (image != null)
+            //    {
+            //        pb.Image = image;
+            //        flowLayoutPanel1.Controls.Add(pb);
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show($"Download image from {imgUrl}");
+            //    }
+
+            //    progressBar1.Value = imageUrls.IndexOf(imgUrl);
+            //    if (progressBar1.Value == progressBar1.Maximum)
+            //    {
+            //        btnExportPDF.Enabled = true;
+            //    }
+            //}
+        }
+    }
+}
